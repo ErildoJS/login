7fcad5215b2944633f951fd00ac47c0ae4cf94ce
ErildoJS <patriciopatrick92@gmail.com>
2020-09-21 17:19:27 +0100

    first commit

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Container, Content} from './styles'
+
+
+const SignIn = () => {
+  return (
+  <Container>
+    <Content>
+      signIn
+    </Content>
+  </Container>
+    )
+}
+
+export default SignIn
---
+++ src/pages/SignIn/styles.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  place-content: center;
+  flex-direction: column;
+  width: 480px;
+  height: 480px;
+  background-color: #907dcf;
+  border-radius: 4px;
+`
